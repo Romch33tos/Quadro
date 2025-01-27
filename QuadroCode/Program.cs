c4113092ae538028817229d7330445c87a6311bf
Romch33tos <Romch33tos@yandex.ru>
2025-01-27 19:26:46 +0700

    Новая версия

--- QuadroCode/Program.cs
+++ QuadroCode/Program.cs
@@ -1,2 +1,26 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http.Headers;
+
+class Programm {
+  static void Main() {
+    
+    Console.Write("Введите значение коэффициента а: ");
+    double a = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Введите значение коэффициента b: ");
+    double b = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Введите значение коэффициента c: ");
+    double c = Convert.ToDouble(Console.ReadLine());
+
+    double D = b * b - 4 * a * c;
+    if (D >= 0) {
+      Console.WriteLine($"Дискриминант уравнения = {D}\nКорни уравнения: ");
+      double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+      double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+      Console.WriteLine($"x1 = {x1}, x2 = {x2}");
+    } else {
+        Console.WriteLine($"Дискриминант уравнения = {D}\nУравнение не имеет корней.");
+      }
+  }
+}
