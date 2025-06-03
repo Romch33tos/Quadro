82b4f56348e77746f96fef6657f9928899492c35
Roman Khadraliev <Romch33tos@yandex.ru>
2025-06-03 13:11:41 +0700

    Изменение графического интерфейса

--- Quadro.py
+++ Quadro.py
@@ -1,119 +1,139 @@
 from tkinter import *
 from tkinter import messagebox as mb
 
-root = Tk()
-root.title("Программа")
-txt = StringVar()
-txt2 = StringVar()
-txt3 = StringVar()
-txt4 = StringVar()
-def clicked():
+def help():
+	answer = mb.showinfo(title = "Справка", message = "Информация")
+	
+def ready():
+	ans.delete(0, END)
 	a = float(a1.get())
 	b = float(b1.get())
 	c  = float(c1.get())
-	if (a == 0) or (b ==0) or (c == 0):
-		answer = mb.showerror(title = "Ошибка" , message ="Убедитесь в правильности введенных данных!")
-	if (a !=0) and (b != 0) and (c !=0) and (b % 2 == 0):
-		answer = mb.showinfo( title="Способы решения", message="Уравнение можно решить через половину коэффициента или дискриминант")
-		Button(root, state = NORMAL,text="Решить через половину коэффициента", command=lambda: clicked3()).grid(row=5,column=0)
-		Button(root, state = NORMAL, text="Решить через дискриминант", command=lambda: clicked2()).grid(row=5, column=1)
-	elif (a !=0) and (b !=0) and (c !=0):
-		answer = mb.showinfo(
-		title="Способы решения",message="Уравнение можно решить через дискриминант" )
-		Button(root, state = DISABLED,text="Решить через половину коэффициента", command=lambda: clicked3()).grid(row=5,column=0)
-		Button(root, state = NORMAL, text="Решить через дискриминант", command=lambda: clicked2()).grid(row=5,column=1)
+	
+	Button(window, state = DISABLED, width = 10, text = "D", command = lambda: discr()).grid(row = 1, column = 0, sticky = SW, padx = 700, pady = 10)
+
+	Button(window, state = DISABLED, width = 10, text = "D/4", command = lambda: halfcoef()).grid(row = 1, column = 0, sticky = SW, padx = 980, pady = 10)
+
+	Button(window, state = DISABLED, width = 10, text = "Теорема Виета", command = lambda: viet).grid(row = 2, column = 0, sticky = SW, padx = 700, pady = 10)
+
+	Button(window, state = DISABLED, width = 10, text = "Переброска", command = lambda: perebros()).grid(row = 2, column = 0, sticky = SW, padx = 980, pady = 10)
+
+	Button(window, state = DISABLED, width = 10, text = "a + b + c = 0", command = lambda: svoistva).grid(row = 3, column = 0, sticky = SW, padx = 700, pady = 10)
+
+	Button(window, state = DISABLED, width = 10, text = "Схема Горнера", command = lambda: gorner()).grid(row = 3, column = 0, sticky = SW, padx = 980, pady = 10)
+
+	Button(window, state = DISABLED, width = 10, text = "Неполное", command = lambda: nepolnoe).grid(row = 4, column = 0, sticky = SW, padx = 840, pady = 10)
+
+	if (a != 0) and ((b == 0) or (c == 0)):
+		Button(window, state = NORMAL, width = 10, text = "Неполное", command = lambda: nepolnoe).grid(row = 4, column = 0, sticky = SW, padx = 840, pady = 10)
 		
-def clicked2():
-    Button(root, state = DISABLED, text="Решить через дискриминант", command=lambda: clicked2()).grid(row=5, column=1)
-    a = float(a1.get())
-    b = float(b1.get())
-    c = float(c1.get())
-    d = (b**2 - 4*a*c)
-    if d >= 0:
-     x1 = ((-b + d**0.5) / (2*a))
-     x2 = ((-b - d**0.5) / (2*a))
-     txt.set("%.2f"%(d))
-     if d > 0:
-     	txt2.set("Уравнение имеет два различных корня")
-     if d == 0:
-     	txt2.set("Уравнение имеет два одинаковых корня")
-     txt3.set("%.2f"%(x1))
-     txt4.set("%.2f"%(x2))
-    else:
-     Button(root, state = DISABLED, text="Решить через дискриминант", command=lambda: clicked2()).grid(row=5, column=1)
-     txt.set("%.2f"%(d))
-     txt2.set("Нет корней")
-def clicked3():
-    Button(root, state = DISABLED, text="Решить через половину коэффициента", command=lambda: clicked3()).grid(row=5, column=0)
-    a = float(a1.get())
-    b = float(b1.get())
-    c = float(c1.get())
-    k = (b / 2)
-    d = (k**2 - a*c)
-    if d >= 0:
-     x1 = ((-k + d**0.5)/ a)
-     x2 = ((-k - d**0.5)/ a)
-     txt.set("%.2f"%(d))
-     if d > 0:
-      	txt2.set("Уравнение имеет два различных корня")
-     if d == 0:
-     	txt2.set("Уравнение имеет два одинаковых корня")
-     txt3.set("%.2f"%(x1))
-     txt4.set("%.2f"%(x2))
-    else:
-     Button(root, state = DISABLED, text="Решить через половину коэффициента", command=lambda: clicked3()).grid(row=5, column=0)
-     txt.set("%.2f"%(d))
-     txt2.set("Нет корней")
+	if (a !=0) and (b !=0) and (c !=0):
+		Button(window, state = NORMAL, width = 10, text = "D", command = lambda: discr()).grid(row = 1, column = 0, sticky = SW, padx = 700, pady = 10)
+		
+	if (a !=0) and (b != 0) and (b % 2 == 0) and (c !=0):
+		
+		Button(window, state = NORMAL, width = 10, text = "D/4", command = lambda: halfcoef()).grid(row = 1, column = 0, sticky = SW, padx = 980, pady = 10)
+		
+	if (a == 1) and (b != 0) and (c != 0):
+		Button(window, state = NORMAL, width = 10, text = "Теорема Виета", command = lambda: viet).grid(row = 2, column = 0, sticky = SW, padx = 700, pady = 10)
+	
+	if (a != 0) and (b != 0) and (c != 0):
+		Button(window, state = NORMAL, width = 10, text = "Переброска", command = lambda: perebros()).grid(row = 2, column = 0, sticky = SW, padx = 980, pady = 10)
+		
+	if (a != 0) and (b != 0) and (c != 0) and ((a + b + c == 0) or ( a - b + c == 0)):
+		Button(window, state = NORMAL, width = 10, text = "a + b + c = 0", command = lambda: svoistva).grid(row = 3, column = 0, sticky = SW, padx = 700, pady = 10)
+		
+	if (a != 0) and (b != 0) and (c != 0):
+		Button(window, state = NORMAL, width = 10, text = "Схема Горнера", command = lambda: gorner()).grid(row = 3, column = 0, sticky = SW, padx = 980, pady = 10)
+		
+	if (a == 0):
+		answer = mb.showerror(title = "Ошибка" , message ="Коэффициент a не может быть равен нулю!")
+		
+def discr():
+	Button(window, state = DISABLED, width = 10, text = "D", command = lambda: discr()).grid(row = 1, column = 0, sticky = SW, padx = 700, pady = 10)
+	a = float(a1.get())
+	b = float(b1.get())
+	c = float(c1.get())
+	d = (b**2 - 4*a*c)
+	if d >= 0:
+	    x1 = ((-b + d**0.5) / (2*a))
+	    x2 = ((-b - d**0.5) / (2*a))
+	    txt.set("D = " + str("%.2f"%(d)) + ". Корни уравнения: х1 = " + str("%.2f"%(x1)) + ", x2 =" + str("%.2f"%(x2)) + ".")
+	else:
+		Button(window, state = DISABLED, width = 10, text = "D", command = lambda: discr()).grid(row = 1, column = 0, sticky = SW, padx = 700, pady = 10)
+		txt.set("D = " + str("%.2f"%(d)) + ". Корней нет. ")
+		
+def halfcoef():
+     	Button(window, state = DISABLED, width = 10, text = "D/4", command = lambda: halfcoef()).grid(row = 1, column = 0, sticky = SW, padx = 980, pady = 10)
+     	a = float(a1.get())
+     	b = float(b1.get())
+     	c = float(c1.get())
+     	k = (b / 2)
+     	d = (k**2 - a*c)
+     	if d >= 0:
+     		x1 = ((-k + d**0.5)/ a)
+     		x2 = ((-k - d**0.5)/ a)
+     		txt.set("D = " + str("%.2f"%(d)) + ". Корни уравнения: х1 = " + str("%.2f"%(x1)) + ", x2 =" + str("%.2f"%(x2)) + ".")
+     	else:
+     	 	Button(window, state = DISABLED, width = 10, text = "D/4", command = lambda: halfcoef()).grid(row = 1, column = 0, sticky = SW, padx = 980, pady = 10)
+     	 	txt.set("D = " + str("%.2f"%(d)) + ". Корней нет. ")
+			
+window = Tk()
+window.title("Программа")
 
-l1 = Label(root, text="Введите значения коэффициентов уравнения")
-l1.grid(row=0, column=0, columnspan=2)
+txt = StringVar()
+txt2 = StringVar()
 
-lA = Label(root, text="a = ")
-lA.grid(row=1, column=0)
+main_menu = Menu()
 
-lB = Label(root, text="b = ")
-lB.grid(row=2, column=0)
+main_menu.add_cascade(label="Справка", command = lambda:help())
 
-lC = Label(root, text="c = ")
-lC.grid(row=3, column=0)
+window.config(menu=main_menu)
 
-a1 = Entry(root)
-a1.grid(row=1, column=1)
+l1 = Label(window, text="Введите значения коэффициентов: ")
+l1.grid(row = 0, column = 0, sticky = NW, padx = 10, pady = 10)
 
-b1 = Entry(root)
-b1.grid(row=2, column=1)
+l2 = Label(window, text="Выберите способ решения: ")
+l2.grid(row = 0, column = 0, sticky = NW, padx = 700, pady = 10)
 
-c1 = Entry(root)
-c1.grid(row=3, column=1)
+l3 = Label(window, text="Коэффициент а =")
+l3.grid(row = 1, column = 0, sticky = NW, padx = 10, pady = 10)
 
-l5 = Label(root, text="Дискриминант уравнения = ")
-l5.grid(row=6, column=0)
+l4 = Label(window, text="Коэффициент b =")
+l4.grid(row = 2, column = 0, sticky = NW, padx = 10, pady = 10)
 
-dis = Entry(root, textvariable=txt)
-dis.grid(row=6, column=1)
+l5 = Label(window, text="Коэффициент c =")
+l5.grid(row = 3, column = 0, sticky = NW, padx = 10, pady = 10)
 
-l6 = Label(root, text="Корни уравнения: ")
-l6.grid(row=7, column=0)
+l6 = Label(window, text = "Решение")
+l6.grid(row = 5, column = 0, sticky = SW, pady = 10, padx = 590)
 
-sol = Label(root, textvariable = txt2)
-sol.grid(row=7, column=1)
+a1 = Entry(window, width = 14, justify = CENTER)
+a1.grid(row = 1, column = 0, sticky = NW, padx = 300, pady = 10)
 
-l7 = Label(root, text="x1 =")
-l7.grid(row=8, column=0)
+b1 = Entry(window, width = 14, justify = CENTER)
+b1.grid(row = 2, column = 0, sticky = NW, padx = 300, pady = 10)
 
-l8 = Label(root, text="x2 =")
-l8.grid(row=9, column=0)
+c1 = Entry(window, width = 14, justify = CENTER)
+c1.grid(row = 3, column = 0, sticky = NW, padx = 300, pady = 10)
 
-x1 = Entry(root, textvariable=txt3)
-x1.grid(row=8, column=1)
+ans = Entry(window, width = 51, textvariable = txt)
+ans.grid(row = 6, column = 0, padx = 125, ipady = 20, pady = 10, sticky = NW)
 
-x2 = Entry(root, textvariable=txt4)
-x2.grid(row=9, column=1)
+Button(window, width = 15, text = "Готово", command = lambda: ready()).grid(row = 4, column = 0, sticky = NW, padx = 125, pady = 10, ipadx = 1)
 
-Button(root, text="Готово", command=lambda: clicked()).grid(row=4, column=1)
+Button(window, state = DISABLED, width = 10, text = "D", command = lambda: discr()).grid(row = 1, column = 0, sticky = SW, padx = 700, pady = 10)
 
-Button(root, state = DISABLED, text="Решить через дискриминант", command=lambda: clicked2()).grid(row=5, column=1)
+Button(window, state = DISABLED, width = 10, text = "D/4", command = lambda: halfcoef()).grid(row = 1, column = 0, sticky = SW, padx = 980, pady = 10)
 
-Button(root, state = DISABLED, text="Решить через половину коэффициента", command=lambda: clicked3()).grid(row=5, column=0)
+Button(window, state = DISABLED, width = 10, text = "Теорема Виета", command = lambda: viet).grid(row = 2, column = 0, sticky = SW, padx = 700, pady = 10)
 
-root.mainloop()
+Button(window, state = DISABLED, width = 10, text = "Переброска", command = lambda: perebros()).grid(row = 2, column = 0, sticky = SW, padx = 980, pady = 10)
+
+Button(window, state = DISABLED, width = 10, text = "a + b + c = 0", command = lambda: svoistva).grid(row = 3, column = 0, sticky = SW, padx = 700, pady = 10)
+
+Button(window, state = DISABLED, width = 10, text = "Схема Горнера", command = lambda: gorner()).grid(row = 3, column = 0, sticky = SW, padx = 980, pady = 10)
+
+Button(window, state = DISABLED, width = 10, text = "Неполное", command = lambda: nepolnoe).grid(row = 4, column = 0, sticky = SW, padx = 840, pady = 10)
+
+window.mainloop()
