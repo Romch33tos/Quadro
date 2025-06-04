062457cb575ea6544190d70e0e877ec6d1299f6a
Roman Khadraliev <Romch33tos@yandex.ru>
2025-06-04 14:04:17 +0700

    Реализованы методы для неполных квадратных уравнений

--- Quadro.py
+++ Quadro.py
@@ -8,7 +8,7 @@ a, b, c, d, x1, x2 = 0, 0, 0, 0, 0, 0
 root = CTk()
 root.title("Приложение")
 root.geometry("600x670")
-root.resizable(width = False, height= False)
+root.resizable(width = False, height = False)
 set_appearance_mode("dark")
 set_default_color_theme("blue")
 
@@ -53,7 +53,7 @@ def block():
     a1.configure(state = DISABLED)
     b1.configure(state = DISABLED)
     c1.configure(state = DISABLED)		
-    
+        
 def ready():
     global d, x1, x2
     block()
@@ -64,7 +64,8 @@ def ready():
             text.insert("1.0", "Коэффициент «а» не может равняться 0!")  
         if ((b == 0) or (c == 0)) and (a != 0):
             text.insert("1.0", "Вы ввели неполное уравнение.")
-            text.insert(END, "\nВыберите метод решения.")     
+            text.insert(END, "\nВыберите метод решения.") 
+            N.configure(state = NORMAL)    
         elif (a != 0 and b != 0 and c!= 0):
             d = b**2 - 4*a*c
             if (a > 0 and c > 0) or (a < 0 and c < 0):
@@ -93,17 +94,20 @@ def ready():
      
      if x3 == x1 or x4 == x2 or Dcheck == (math.sqrt(d)):
          P.configure(state = NORMAL)
+         
      if (a == 1) and (b != 0) and (c != 0) and d >= 0:
          x3 = int(x1)
          x4 = int(x2)
          if x3 == x1 and x4 == x2:
              V.configure(state = NORMAL)
+             
      if (a != 0) and (b != 0) and (c != 0)  and ((a + b + c == 0) or (a - b + c == 0)) and d >= 0:
          S.configure(state = NORMAL)
+         
      if (a !=0) and (b != 0) and (b % 2 == 0) and (c !=0) and d >= 0:
          D4.configure(state = NORMAL)
 		
-def Discriminant():
+def discriminant():
 	clear_text()
 	get_entry()	
 	if (a > 0 and c > 0) or (a < 0 and c < 0):
@@ -111,11 +115,11 @@ def Discriminant():
 	elif ((a < 0) ^ (c < 0)):
 	  text.insert("1.0", f"D = {b}² + 4 × {abs(a)} x {abs(c)}  = {b**2} + {abs(4 * a * c)} = {d}")
 	text.insert(END, "\nПо формуле корней:")	
-	text.insert(END, f"\nх1 = ({-b} + {int(math.sqrt(d))}) / (2 × {a}) = {int(-b + math.sqrt(d))} / {2 * a} = {int(x1)}")
-	text.insert(END, f"\nх2 = ({-b} - {int(math.sqrt(d))}) / (2 × {a}) = {int(-b - math.sqrt(d))} / {2 * a} = {int(x2)}")	
+	text.insert(END, f"\nх1 = ({-b} + {int(math.sqrt(d))}) / (2 × {a}) = {int(-b + math.sqrt(d))} / {2 * a} = {x1:.2f}")
+	text.insert(END, f"\nх2 = ({-b} - {int(math.sqrt(d))}) / (2 × {a}) = {int(-b - math.sqrt(d))} / {2 * a} = {x2:.2f}")	
 	text.configure(state = DISABLED)
 	
-def half():
+def halfOfTheCoefficient():
 	clear_text()
 	get_entry()	
 	k = int(b/2)
@@ -127,8 +131,8 @@ def half():
 	elif ((a < 0) ^ (c < 0)):
 	  text.insert("1.0", f"k = {k}, D/4 = {k}² + {abs(a)} x {abs(c)}  = {k**2} + {abs(a * c)} = {d}")
 	text.insert(END, "\nЧерез половину коэффициента: ")	
-	text.insert(END, f"\nх1 = ({-k} + {int(math.sqrt(d))}) / {a} = {int(-k + math.sqrt(d))} / { a} = {int(x1)}")
-	text.insert(END, f"\nх2 = ({-k} - {int(math.sqrt(d))}) / {a} = {int(-k - math.sqrt(d))} / {a} = {int(x2)}")	
+	text.insert(END, f"\nх1 = ({-k} + {int(math.sqrt(d))}) / {a} = {int(-k + math.sqrt(d))} / { a} = {x1:.2f}")
+	text.insert(END, f"\nх2 = ({-k} - {int(math.sqrt(d))}) / {a} = {int(-k - math.sqrt(d))} / {a} = {x2:.2f}")	
 	text.configure(state = DISABLED)
 	
 def Viet():
@@ -140,15 +144,14 @@ def Viet():
 	text.insert(END, f"\nx1 = {int(x1)}, x2 = {int(x2)}")
 	text.configure(state = DISABLED)
 	
-def Perebroska():
+def perebroska():
 	clear_text()
 	get_entry()	
 	text.insert("1.0", "Методом переброски коэффициента: ")
 	text.insert(END, f"\nx1 + x2 = {-b}")
 	text.insert(END, f"\nx1 × x2 = {c * a}")
-	text.insert(END, f"\nx1 = {int(x1 * a)} / {a} = {int(x1)}, x2 = {int(x2 * a)} / {a} = {int(x2)}")
-	text.configure(state = DISABLED)
-
+	text.insert(END, f"\nx1 = {int(x1 * a)} / {a} = {x1:.2f}, x2 = {int(x2 * a)} / {a} = {x2:.2f}")
+	text.configure(state = DISABLED)						   			   
 def svoistva():
     clear_text()
     get_entry()   
@@ -162,7 +165,65 @@ def svoistva():
     		text.insert(END, "\nx1 = -1")
     		text.insert(END, f"\nx2 = -c / a = {-c} / {a} = {int(-c/a)}")
     text.configure(state = DISABLED)
-    		
+    
+a_sign = ""
+b_sign = ""
+sign = ""
+    
+def replacement():
+	global a, b, c, a_sign, sign,b_sign
+	get_entry()
+	clear_text()	
+	if a == 1:
+	    a_sign = ""
+	elif a == -1:
+	    a_sign = "-"
+	else:
+	    a_sign = a
+	if b == 1 or b == -1:
+	    b_sign = ""
+	else:
+	    b_sign = abs(b)
+	if c > 0 or b > 0:
+	    sign = "+"
+	elif c < 0 or b < 0:
+	    sign = "-"		
+	
+def nepolnoe():
+	global sign, a_sign, b_sign
+	replacement()
+	get_entry()
+	clear_text()		
+	if (c != 0) and (-c/a < 0) and (b == 0):
+		text.insert("1.0", f"{a_sign}x² {sign} {abs(c)} = 0")
+		text.insert(END, f"\nx² = {int(-c/a)}")
+		text.insert(END, "\nНет корней!")
+		
+	if (c != 0) and (-c/a > 0) and (b == 0):
+		text.insert("1.0", f"{a_sign}x² {sign} {abs(c)} = 0")
+		text.insert(END, f"\nx² = {int(-c/a)}")
+		text.insert(END, f"\nx1 = {int(math.sqrt(-c/a))}")
+		text.insert(END, f"\nx2 = {-int(math.sqrt(-c/a))}")
+			
+	if (c == 0) and (b != 0) and (a != 0):		
+		text.insert("1.0", f"{a_sign}x² {sign} {b_sign}x = 0")
+		if (a < 0) and (b < 0):
+		    sign = "+"
+		    a_sign = abs(a)
+		if (a < 0) and (b > 0):
+		    sign = "-"		
+		    a_sign = abs(a)
+		if (a == -1):
+		    a_sign = ""
+		text.insert(END, f"\n{a_sign}x × (x  {sign} {abs(int(b/a))}) = 0")
+		text.insert(END, f"\n{a_sign}x = 0, (x {sign} {abs(int(b/a))}) = 0")
+		text.insert(END, f"\nx1 = 0, x2 = {int(-b/a)}")
+				
+	if (a != 0) and (b == 0) and (c == 0):
+		text.insert("1.0", f"{a_sign}x² = 0")
+		text.insert(END, "\nx = 0")
+	text.configure(state = DISABLED)
+		
 text = CTkTextbox(root, width = 567, height = 160, wrap = WORD, font = ("Arial", 29))
 text.grid(row = 0, column = 0, padx = 10,pady = 7, sticky = NW, ipadx = 5)
 text.insert("1.0", "Введите коэффициенты уравнения")
@@ -196,22 +257,22 @@ CLR.grid(row = 5, column = 0, sticky = NW, padx = 310, pady = 5,ipady = 5, ipadx
 l6 = CTkLabel(root, text = "Методы решения:",font = ("Arial", 30))
 l6.grid(row = 6, column = 0, sticky = NW, padx = 10, pady = 5)
 
-D = CTkButton(root, width = 255, text = "D", command = lambda: Discriminant(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
+D = CTkButton(root, width = 255, text = "D", command = lambda: discriminant(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
 D.grid(row = 7, column = 0, sticky = NW, padx = 10, pady = 5,ipady = 5, ipadx = 10)
 
-D4 = CTkButton(root, width = 255, text = "D/4", command = lambda: half(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
+D4 = CTkButton(root, width = 255, text = "D/4", command = lambda: halfOfTheCoefficient(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
 D4.grid(row = 7, column = 0, sticky = NW, padx = 310, pady = 5,ipady = 5, ipadx = 10)
 
 V = CTkButton(root, width = 255, text = "Теорема Виета", command = lambda: Viet(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
 V.grid(row = 8, column = 0, sticky = NW, padx = 10, pady = 5,ipady = 5, ipadx = 10)
 
-P = CTkButton(root, width = 255, text = "Переброска", command = lambda: Perebroska(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
+P = CTkButton(root, width = 255, text = "Переброска", command = lambda: perebroska(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
 P.grid(row = 8, column = 0, sticky = NW, padx = 310, pady = 5,ipady = 5, ipadx = 10)
 
 S = CTkButton(root, width = 255, text = "a ± b + c = 0", command = lambda: svoistva(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
 S.grid(row = 9, column = 0, sticky = NW, padx = 10, pady = 5,ipady = 5, ipadx = 10)
 
-N = CTkButton(root, width = 255, text = "Неполное", command = lambda: Discriminant(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
+N = CTkButton(root, width = 255, text = "Неполное", command = lambda: nepolnoe(), font = ("Arial", 30), corner_radius = 10, height = 50, state = DISABLED)
 N.grid(row = 9, column = 0, sticky = NW, padx = 310, pady = 5,ipady = 5, ipadx = 10)
 
 root.mainloop()
